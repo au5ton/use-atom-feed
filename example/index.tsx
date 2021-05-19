4c927bd7f32b22505976f7f8bf9ce3df97009e9e
Austin Jackson <themacphage@gmail.com>
2021-05-19 18:35:34 -0500

    example app working

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,19 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { useAtomFeed } from '../.';
+
+import sampleFeed from 'url:/samples/Newtonsoft.Json.atom.xml';
 
 const App = () => {
+  const { data, error, isValidating } = useAtomFeed(sampleFeed);
+  console.log('sampleFeed: ', data);
   return (
     <div>
-      <Thing />
+      <h1>Hello, World!</h1>
+      <pre>
+        {data !== undefined ? JSON.stringify(data, undefined, 2): ''}
+      </pre>
     </div>
   );
 };
