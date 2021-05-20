1ccea653f5e2076e8c8b769f39fd3861b660bd7b
Austin Jackson <themacphage@gmail.com>
2021-05-20 02:07:05 -0500

    add complex test for blog

--- test/blog.test.tsx
+++ test/blog.test.tsx
@@ -1,13 +1,40 @@
-import { Guards, Parser } from '../src';
+import { AtomAuthor, AtomLink, AtomTitle, Guards, Parser } from '../src';
 import { readFileSync } from './util';
 
 describe('blog', () => {
   const data = readFileSync('samples', 'blog.cougargrades.io', 'blog.atom.xml');
-  it('parseAtomFeed', () => {
+  it('call parseAtomFeed() without crashing', () => {
     Parser.parseAtomFeed(data);
   });
-  it('isAtomFeed', () => {
+  it('type guard sanity check (isAtomFeed)', () => {
     expect(Guards.isAtomFeed(Parser.parseAtomFeed(data))).toBeTruthy();
   });
-  // TODO: verify that the output looks like it's supposed to
+  it('has expected feed info', () => {
+    const feed = Parser.parseAtomFeed(data);
+    expect(feed.id).toEqual<string>('https://blog.cougargrades.io');
+    expect(feed.title).toEqual<AtomTitle>({ type: undefined, value: 'CougarGrades.io' });
+    expect(feed.updated instanceof Date).toBe(true);
+    expect(Array.isArray(feed.entries)).toBe(true);
+    expect(feed.author).toContainEqual<AtomAuthor>({
+      name: 'Austin Jackson',
+      email: 'austinjckson@gmail.com',
+      uri: undefined
+    });
+    expect(feed.link).toContainEqual<AtomLink>({
+      href: 'https://blog.cougargrades.io/atom.xml',
+      rel: 'self',
+      type: undefined,
+      hreflang: undefined,
+      title: undefined,
+      length: undefined
+    });
+    expect(feed.link).toContainEqual<AtomLink>({
+      href: 'https://blog.cougargrades.io/',
+      rel: undefined,
+      type: undefined,
+      hreflang: undefined,
+      title: undefined,
+      length: undefined
+    });
+  });
 });
