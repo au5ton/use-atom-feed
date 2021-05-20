7386757de4f79d2ef94aaf35a07df19a34c9d6eb
Austin Jackson <themacphage@gmail.com>
2021-05-20 00:18:00 -0500

    removed in-house stuff and other crud

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-export * from './AtomFeed';
-export * from './AtomEntry';
-export * from './AtomCommon';
-export * as Parser from './Parser';
-export * as Guards from './index.guard';
-export { useAtomFeed } from './useAtomFeed';
+export { useAtomFeed, Feed } from './useAtomFeed';
